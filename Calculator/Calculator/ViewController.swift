c5da93d0873dacf98bf13cbc11bef8ebe906bafe
Jameka Echols <jameka.echols@gmail.com>
2020-09-14 20:49:08 -0500

    initial commit before something goes wrong

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Jameka Echols on 9/10/20.
+//  Copyright © 2020 CutThroatTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
